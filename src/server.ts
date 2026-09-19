e515e55d7fd8ba3382925436ba18526c0ffa95ca
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-09-19 23:07:20 +0600

    3rd commit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app"
+import config from "./config";
 import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT||3000;
+const PORT = config.port
 
 
 async function main(){
