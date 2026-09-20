db37059dc4593a4954a0010b8bd8cfc676fb1655
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-09-20 10:09:43 +0600

    5th commit

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cookieParser from "cookie-parser";
 import express,{ Application, Request, Response } from "express";
 import cors from 'cors'
 import config from "./config";
-
+import httpStatus from 'http-status'
 
 
 const app:Application = express()
@@ -20,6 +20,17 @@ app.use(cookieParser())
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("Hello world!!")
+})
+
+app.post('/api/users/register',async(req:Request,res:Response)=>{
+    const payload = req.body
+
+    console.log(payload)
+
+    res.status(httpStatus.CREATED).json({
+        message:"user register successfully!!"
+    })
+
 })
 
 
